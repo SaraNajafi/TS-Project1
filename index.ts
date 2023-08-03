bf1bd5973f05d5004f0d07aee7106bf49dd03ee3
Sara Najafi <sara.njf.ch@gmail.com>
2023-08-03 15:32:37 +0330

    deleteLabel
    +Define a method to delete a label from a task.

--- index.ts
+++ index.ts
@@ -35,6 +35,22 @@ class Tasks{
 
     }
 
+
+    // isTaskExist(title:String){
+    //     let message:string =""
+    //     const index = this.tasksArray.findIndex(task => task.title === title);
+    //     if(index === -1){
+    //         message =`Task with this title ${title} does not exist!`;
+
+    //     }else{
+    //         this.tasksArray.splice(index, 1);
+    //         message=`Task with title ${title} deleted successfully!`;
+    //     }
+    //     return message;
+    // }
+
+
+
     deleteTaskByTitle(title: String){
         let message:string =""
         const index = this.tasksArray.findIndex(task => task.title === title);
@@ -65,3 +81,22 @@ class Tasks{
 
 }
 
+    deleteLabel(label: label, title:String){
+        let message="";
+        const index = this.tasksArray.findIndex(task => task.title === title);
+        if(index === -1){
+            message= `Task with title ${title} already exists`;
+        }else{
+            const task = this.tasksArray[index];
+            const indexForRemove = task?.lable.indexOf(label);
+            if(indexForRemove===undefined){
+                message= `Task with title ${title} has no label ${label}`;
+            }else{
+            task?.lable.splice(indexForRemove,1);
+            }
+
+    }
+
+
+
+}
